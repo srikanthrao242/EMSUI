2424c04b56c2b58eb59adad7cdb85d34d57899f1
srikanthrao242 <srikanthraomca@yahoo.com>
2020-03-09 00:35:38 +0530

    table for exams

--- src/app/student-examinations/student-examinations.component.ts
+++ src/app/student-examinations/student-examinations.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import Academic from 'src/app/models/academics';
 import { AcademicService } from 'src/app/academic/academic.service';
+import { StudentExaminationsService } from './student-examinations.service';
+import { NotificationService } from '../toastr-notification/toastr-notification.service';
 
 @Component({
   selector: 'app-student-examinations',
@@ -10,8 +12,13 @@ import { AcademicService } from 'src/app/academic/academic.service';
 export class StudentExaminationsComponent implements OnInit {
   academicsNames: Academic[];
 
+  selectedAcademic:string;
+  examData:any;
+
   constructor(
-    private academicService: AcademicService) { }
+    private academicService: AcademicService,
+    private notifications: NotificationService,
+    private studentExamSevices : StudentExaminationsService) { }
 
   ngOnInit() {
     this.academicService.getAllAcademicNames().subscribe(
@@ -19,5 +26,21 @@ export class StudentExaminationsComponent implements OnInit {
       error => console.error('There was an error!', error)
     );
   }
+
+  onSubmitAcademic(){
+    this.studentExamSevices.getExamData(+this.selectedAcademic)
+    .subscribe(
+      data => {
+          this.examData = data;
+      },
+      error => {
+        this.notifications.error(`Got error while getting exam data`);
+      });
+  }
+  startEdit(num:number, ExamID:string, ExamName:string, ExamFor:string, TotalMarks:String, ExamDate:string){
+
+  }
+
+
 
 }
--- src/app/student-examinations/student-examinations.service.ts
+++ src/app/student-examinations/student-examinations.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { config } from '../Config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentExaminationsService {
 
-  constructor() { }
+  user = JSON.parse(localStorage.getItem('currentUser'));
+
+  examURL = `${config.server.serverURL}/api/examination/${this.user.id}/exams`
+
+  constructor(private http: HttpClient) { }
+
+  getExamData(id:number) {
+    return this.http.get(`${this.examURL}/${id}`);
+  }
+
 }
